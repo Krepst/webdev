b401e3adb507677daa23bca7e111d94a40909cdb
Krepst <oloocram@protonmail.com>
2022-02-19 00:40:46 +0200

    switches

--- drumroll/index.js
+++ drumroll/index.js
@@ -6,80 +6,54 @@ document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
     makeSound(buttonInnerHTML);
     buttonAnimation(buttonInnerHTML);
-
-   switch (buttonInnerHTML) {
-       case "w":
-           var tom1 =  new Audio("sounds/tom-1.mp3");
-           tom1.play();
-           break;
-        case "a":
-            var tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play();
-            break;
-        case "s":
-            var tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();
-            break;
-        case "d":
-            var tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();
-            break;
-        case "j":
-            var kick = new Audio("sounds/kick-bass.mp3");
-            kick.play();
-            break;
-        case "k":
-            var snare = new Audio("sounds/snare.mp3");
-            snare.play();
-            break;
-        case "l":
-            var crash = new Audio("sounds/crash.mp3");
-            crash.play();
-            break;
-   
-       default: console.log(innerHTML);
-           break;
-   }
-});
+})
+}
+  
 
 document.addEventListener("keydown", function(event) {
 
     makeSound(event.key);
-    buttonAnimation(event.key);
 
 })
 
-}
+
 function makeSound(key) {
 
     switch (key) {
         case "w":
             var tom1 =  new Audio("sounds/tom-1.mp3");
             tom1.play();
+            document.querySelector(".w").classList.toggle("pressed");
             break;
          case "a":
              var tom2 = new Audio("sounds/tom-2.mp3");
              tom2.play();
+             document.querySelector(".a").classList.toggle("pressed");
              break;
          case "s":
              var tom3 = new Audio("sounds/tom-3.mp3");
              tom3.play();
+             document.querySelector(".s").classList.toggle("pressed");
              break;
          case "d":
              var tom4 = new Audio("sounds/tom-4.mp3");
              tom4.play();
+             document.querySelector(".d").classList.toggle("pressed");
              break;
          case "j":
              var kick = new Audio("sounds/kick-bass.mp3");
              kick.play();
+             document.querySelector(".j").classList.toggle("pressed");
              break;
          case "k":
              var snare = new Audio("sounds/snare.mp3");
              snare.play();
+             document.querySelector(".k").classList.toggle("pressed");
              break;
          case "l":
              var crash = new Audio("sounds/crash.mp3");
              crash.play();
+             document.querySelector(".l").classList.toggle("pressed");
              break;
     
         default: console.log(key);
@@ -88,9 +62,3 @@ function makeSound(key) {
 }
 
 
-function buttonAnimation(currentKey) {
-    var activeButton = document.querySelector("." + currentKey);
-    class activeButton {
-        constructor()
-    }
-}
