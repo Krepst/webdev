4b083b2c45b621292b9ba963f08a0b57483aaa0d
Krepst <oloocram@protonmail.com>
2022-03-07 17:21:41 +0200

    added toggle buttons

--- drumroll/index.js
+++ drumroll/index.js
@@ -6,15 +6,16 @@ document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
     makeSound(buttonInnerHTML);
     buttonAnimation(buttonInnerHTML);
-})
+});
 }
   
 
 document.addEventListener("keydown", function(event) {
 
     makeSound(event.key);
+    buttonAnimation(event.key);
 
-})
+});
 
 
 function makeSound(key) {
@@ -23,42 +24,41 @@ function makeSound(key) {
         case "w":
             var tom1 =  new Audio("sounds/tom-1.mp3");
             tom1.play();
-            document.querySelector(".w").classList.toggle("pressed");
             break;
          case "a":
              var tom2 = new Audio("sounds/tom-2.mp3");
              tom2.play();
-             document.querySelector(".a").classList.toggle("pressed");
              break;
          case "s":
              var tom3 = new Audio("sounds/tom-3.mp3");
              tom3.play();
-             document.querySelector(".s").classList.toggle("pressed");
              break;
          case "d":
              var tom4 = new Audio("sounds/tom-4.mp3");
              tom4.play();
-             document.querySelector(".d").classList.toggle("pressed");
              break;
          case "j":
              var kick = new Audio("sounds/kick-bass.mp3");
              kick.play();
-             document.querySelector(".j").classList.toggle("pressed");
              break;
          case "k":
              var snare = new Audio("sounds/snare.mp3");
              snare.play();
-             document.querySelector(".k").classList.toggle("pressed");
              break;
          case "l":
              var crash = new Audio("sounds/crash.mp3");
              crash.play();
-             document.querySelector(".l").classList.toggle("pressed");
              break;
     
         default: console.log(key);
-            break;
     }
 }
 
-
+  function buttonAnimation(currentKey) {
+                var activeButton = document.querySelector("." + currentKey);
+                 activeButton.classList.toggle("pressed");
+                
+                 setTimeout (function() {
+                     activeButton.classList.toggle("pressed");
+             }, 100);
+   }
